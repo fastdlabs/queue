3ef89e3e73cc43fae2e1ea9f9d3d0e2d251fe970
wangjian <1636801376@qq.com>
2018-08-08 20:27:28 +0800

    fix

--- src/Console/StartCommand.php
+++ src/Console/StartCommand.php
@@ -4,6 +4,7 @@ namespace FastD\Queue\Console;
 use FastD\Queue\Traits\LoadConfig;
 use Wangjian\Queue\Console\StartCommand as BaseStartCommand;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class StartCommand extends BaseStartCommand
 {
@@ -13,6 +14,7 @@ class StartCommand extends BaseStartCommand
     {
         $this->setName('queue:start')
             ->setDescription('start the queue consumer worker')
-            ->addArgument('name', InputArgument::OPTIONAL, 'the worker name', app()->getName());
+            ->addArgument('name', InputArgument::OPTIONAL, 'the worker name', app()->getName())
+            ->addOption('bootstrap', 'b', InputOption::VALUE_OPTIONAL, 'bootstrap file');
     }
 }
